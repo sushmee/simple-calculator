13598b6cef32e9017fdd180a3efea115967593b3
sushmee <86013254+sushmee@users.noreply.github.com>
2023-01-15 12:42:20 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Appp from './Appp';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( < Appp / > );
